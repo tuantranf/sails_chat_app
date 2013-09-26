0c06dbedba7f90d510e1f985b5e30cc4334aa550
tuantm <tuandongdu@gmail.com>
2013-09-27 01:26:37 +0900

    update message process

--- api/models/Messages.js
+++ api/models/Messages.js
@@ -9,7 +9,7 @@
 module.exports = {
 
 	attributes : {
-		userId: 'INT',
+		userId: 'STRING',
 		username: 'STRING',
 		message: 'STRING'
 	}
--- api/policies/MessagesPolicy.js
+++ api/policies/MessagesPolicy.js
@@ -1,10 +1,8 @@
 module.exports = function (req, res, next) {
-		console.log(req.session.user)
 
 	if (req.session.user) {
-			var action = req.target.action;
+		var action = req.target.action;
 		if (action === "create") {
-			console.log('asdf')
 			req.body.userId = req.session.user.id;
 			req.body.username = req.session.user.username;
 		}
