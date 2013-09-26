14591549ba8570eb1e9999fba98cc11f19bea367
Tran Minh Tuan <tran_minh_tuan@cyberagent.co.jp>
2013-09-26 20:05:31 +0900

    add sails-mongo package

--- config/adapters.js
+++ config/adapters.js
@@ -1,8 +1,8 @@
 /**
  * Global adapter config
- * 
+ *
  * The `adapters` configuration object lets you create different global "saved settings"
- * that you can mix and match in your models.  The `default` option indicates which 
+ * that you can mix and match in your models.  The `default` option indicates which
  * "saved setting" should be used if a model doesn't have an adapter specified.
  *
  * Keep in mind that options you define directly in your model definitions
@@ -14,31 +14,36 @@
 
 module.exports.adapters = {
 
-  // If you leave the adapter config unspecified 
-  // in a model definition, 'default' will be used.
-  'default': 'disk',
+	mongo: {
+		module: 'sails-mongo',
+		url: process.env.MONGOHQ_URL || 'mongodb://localhost:27017/sails'
+	},
 
-  // In-memory adapter for DEVELOPMENT ONLY
-  memory: {
-    module: 'sails-memory'
-  },
+	// In-memory adapter for DEVELOPMENT ONLY
+	// (data is NOT preserved when the server shuts down)
+	memory: {
+		module: 'sails-dirty',
+		inMemory: true
+	},
 
-  // Persistent adapter for DEVELOPMENT ONLY
-  // (data IS preserved when the server shuts down)
-  disk: {
-    module: 'sails-disk'
-  },
+	// Persistent adapter for DEVELOPMENT ONLY
+	// (data IS preserved when the server shuts down)
+	// PLEASE NOTE: disk adapter not compatible with node v0.10.0 currently
+	//				because of limitations in node-dirty
+	//				See https://github.com/felixge/node-dirty/issues/34
+	disk: {
+		module: 'sails-dirty',
+		filePath: './.tmp/dirty.db',
+		inMemory: false
+	},
 
-  // MySQL is the world's most popular relational database.
-  // Learn more: http://en.wikipedia.org/wiki/MySQL
-  mysql: {
-
-    module: 'sails-mysql',
-    host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-    user: 'YOUR_MYSQL_USER',
-    // Psst.. You can put your password in config/local.js instead
-    // so you don't inadvertently push it up if you're using version control
-    password: 'YOUR_MYSQL_PASSWORD', 
-    database: 'YOUR_MYSQL_DB'
-  }
+	// MySQL is the world's most popular relational database.
+	// Learn more: http://en.wikipedia.org/wiki/MySQL
+	mysql: {
+		module		: 'sails-mysql',
+		host		: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
+		user		: 'YOUR_MYSQL_USER',
+		password	: 'YOUR_MYSQL_PASSWORD',
+		database	: 'YOUR_MYSQL_DB'
+	}
 };
